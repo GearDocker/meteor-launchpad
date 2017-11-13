c4ce4fa3d7822a790d6910c96706ac1b675a2541
Gary <gear@thytruth.com>
2017-11-13 19:38:16 +0800

    hardwired meteor version to 1.4.2.3

--- scripts/build-meteor.sh
+++ scripts/build-meteor.sh
@@ -23,6 +23,9 @@ VERSION=`meteor --version --allow-superuser`
 echo "Meteror version is $VERSION"
 sleep 4
 
+printf "\n[-] Resetting meteor ...\n\n"
+meteor reset --allow-superuser
+
 # Install app deps
 printf "\n[-] Running npm install --allow-superuser in app directory at $APP_BUNDLE_DIR ...\n\n"
 meteor npm install --allow-superuser
